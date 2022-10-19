b446953106573936c4a601112640af5eeaefdc8d
Azzedde <ja_aitsaid@esi.dz>
2022-10-20 00:06:11 +0100

    Adding official enemies, decrease speed

--- enemy.py
+++ enemy.py
@@ -2,15 +2,18 @@ import pygame
 import os
 import random
 
+
 class Enemy(pygame.sprite.Sprite):
+    
     def __init__(self):
+        i = random.randint(1,3)
         super(Enemy, self).__init__()
-        self.image = pygame.image.load(os.path.join('Assets','enemy.png')).convert_alpha()
+        self.image = pygame.image.load(os.path.join('Assets','enemy'+str(i)+'.png')).convert_alpha()
         self.image = pygame.transform.scale(self.image, (52,52))
         self.rect = self.image.get_rect()
         self.rect.x = 800
         self.rect.y = random.randrange(5,420)
-        self.speed_x = random.randrange(18,24)
+        self.speed_x = random.randrange(9,18)
         self.speed_y = 0
 
     def update(self):
